3112accb7d76591948f3a958791d105d0a9d0e88
Larry <lwmccarty@gmail.com>
2018-09-18 02:02:26 -0700

    Call show image in presenter

--- app/src/main/java/com/mccarty/cloudcam/ui/main/MainFragment.java
+++ app/src/main/java/com/mccarty/cloudcam/ui/main/MainFragment.java
@@ -8,11 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.GridView;
+
 import com.mccarty.cloudcam.R;
 import com.mccarty.cloudcam.di.component.ActivityScope;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import dagger.android.DaggerFragment;
 
 @ActivityScope
@@ -20,6 +25,11 @@ public class MainFragment extends DaggerFragment implements MainContract.MainVie
 
     @Inject
     MainPresenterImpl presenter;
+
+    @BindView(R.id.image_grid)
+    GridView gridView;
+
+    private Unbinder unbinder;
 
     @Inject
     public MainFragment() {
@@ -35,7 +45,9 @@ public class MainFragment extends DaggerFragment implements MainContract.MainVie
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
@@ -48,12 +60,18 @@ public class MainFragment extends DaggerFragment implements MainContract.MainVie
     @Override
     public void onDestroy() {
         super.onDestroy();
+        unbinder.unbind();
         presenter.dropView();
     }
 
     @Override
     public void loadImages() {
         // TODO:
+    }
+
+    @Override
+    public void imageClicked() {
+        presenter.showImage();
     }
 
     @Override
