bffd54f95dfe7cb3d9003c01989f94a486c03ff6
Larry <lwmccarty@gmail.com>
2018-10-04 01:08:55 -0700

    Start work on login

--- app/src/main/java/com/mccarty/cloudcam/ui/login/LoginActivity.java
+++ app/src/main/java/com/mccarty/cloudcam/ui/login/LoginActivity.java
@@ -1,58 +1,24 @@
 package com.mccarty.cloudcam.ui.login;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.amazonaws.mobile.auth.core.DefaultSignInResultHandler;
-import com.amazonaws.mobile.auth.core.IdentityManager;
-import com.amazonaws.mobile.auth.core.IdentityProvider;
-import com.amazonaws.mobile.auth.ui.AuthUIConfiguration;
-import com.amazonaws.mobile.auth.ui.SignInActivity;
-import com.amazonaws.mobile.auth.ui.SignInView;
+import com.amazonaws.mobile.auth.ui.SignInUI;
 import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.AWSStartupHandler;
-import com.amazonaws.mobile.client.AWSStartupResult;
 import com.mccarty.cloudcam.R;
-import com.mccarty.cloudcam.awsutils.AWSProvider;
-import com.mccarty.cloudcam.ui.base.BaseActivity;
 import com.mccarty.cloudcam.ui.main.MainActivity;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        AWSMobileClient.getInstance().initialize(this, awsStartupResult -> {
+           SignInUI signInUI = (SignInUI) AWSMobileClient.getInstance().getClient(
+                    LoginActivity.this, SignInUI.class);
+            signInUI.login(LoginActivity.this, MainActivity.class).execute();
+        });
 
-
-        // Start the authentication UI
-       /* AuthUIConfiguration config = new AuthUIConfiguration.Builder()
-                .userPools(true)
-                .build();
-        SignInActivity.startSignInActivity(this, config);
-        LoginActivity.this.finish();*/
-    }/*    final IdentityManager identityManager = AWSProvider.getInstance().getIdentityManager();
-        // Set up the callbacks to handle the authentication response
-        identityManager.setUpToAuthenticate(this, new DefaultSignInResultHandler() {
-            @Override
-            public void onSuccess(Activity activity, IdentityProvider identityProvider) {
-                Toast.makeText(LoginActivity.this,
-                        String.format("Logged in as %s", identityManager.getCachedUserID()),
-                        Toast.LENGTH_LONG).show();
-                // Go to the main activity
-                final Intent intent = new Intent(activity, MainActivity.class)
-                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                activity.startActivity(intent);
-                activity.finish();
-            }
-
-            @Override
-            public boolean onCancel(Activity activity) {
-                return false;
-            }
-        });*/
+    }
 }
