b36db1173ce6543b922899f1f3a2f5a2cac7d886
Larry <lwmccarty@gmail.com>
2018-10-04 01:10:26 -0700

    Adjust image size

--- app/src/main/java/com/mccarty/cloudcam/utils/UIUtils.java
+++ app/src/main/java/com/mccarty/cloudcam/utils/UIUtils.java
@@ -7,24 +7,21 @@ import java.time.format.DateTimeFormatter;
 import static com.mccarty.cloudcam.di.module.ModuleConstants.JPEG_EXTENSION;
 
 public class UIUtils {
+
     public static String appendToImage() {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH.mm.ss");
         return now.format(formatter) + JPEG_EXTENSION;
     }
 
-    public static float getThumbnailHeight(Resources resources) {
-        int div = 3;
-        float percent = 0.93f;
-        float density = 160f;
+    public static int getThumbnailHeightPx(Resources resources) {
         float dp;
         if (resources.getConfiguration().orientation == 1) {
-            dp = (resources.getDisplayMetrics().widthPixels / (resources.getDisplayMetrics().densityDpi / density) / div) * percent;
+            dp = resources.getDisplayMetrics().widthPixels / resources.getDisplayMetrics().density;
         } else {
-            dp = (resources.getDisplayMetrics().heightPixels / (resources.getDisplayMetrics().densityDpi / density) / div)  * percent;
+            dp = resources.getDisplayMetrics().heightPixels / resources.getDisplayMetrics().density;
         }
-
-        return dp;
+        return Math.round((dp * (resources.getDisplayMetrics().densityDpi / 160f)) / 3.07f);
     }
 
 }
