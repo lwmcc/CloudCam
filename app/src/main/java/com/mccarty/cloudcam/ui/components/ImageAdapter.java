f93c27b0eb1ca4b33e49eeabb15bab3be0a1c5c3
Larry <lwmccarty@gmail.com>
2018-10-04 01:13:07 -0700

    Adjust Glide settings

--- app/src/main/java/com/mccarty/cloudcam/ui/components/ImageAdapter.java
+++ app/src/main/java/com/mccarty/cloudcam/ui/components/ImageAdapter.java
@@ -1,23 +1,24 @@
 package com.mccarty.cloudcam.ui.components;
 
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.mccarty.cloudcam.R;
-
-import java.io.File;
 import java.util.List;
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
     private List<String> data;
+    private int thumbSize;
 
-    public ImageAdapter(List<String> data) {
+    public ImageAdapter(List<String> data, int thumbSize) {
         this.data = data;
+        this.thumbSize = thumbSize;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -32,13 +33,15 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         ImageView v =(ImageView) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.grid_item_layout, parent, false);
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+        return  new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Glide.with(holder.image).load(data.get(position)).into(holder.image);
+        Glide.with(holder.image).load(data.get(position)).
+                apply(new RequestOptions().override(thumbSize, thumbSize).
+                centerCrop().placeholder(R.drawable.ic_panorama)).
+                into(holder.image);
     }
 
     @Override
