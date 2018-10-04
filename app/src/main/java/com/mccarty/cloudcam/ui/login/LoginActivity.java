9312d726768ba83266d7c487536bd63f561db541
Larry <lwmccarty@gmail.com>
2018-10-04 01:12:32 -0700

    add signin

--- app/src/main/java/com/mccarty/cloudcam/ui/login/LoginActivity.java
+++ app/src/main/java/com/mccarty/cloudcam/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.amazonaws.mobile.auth.ui.SignInUI;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.mccarty.cloudcam.R;
+
 import com.mccarty.cloudcam.ui.main.MainActivity;
 
 public class LoginActivity extends Activity {
