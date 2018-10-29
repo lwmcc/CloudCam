4567be8f8fa5e72783346464ddb598338f94f66b
Larry <lwmccarty@gmail.com>
2018-10-28 20:30:25 -0700

    Add AWS S3 path

---
+++ app/src/main/java/com/mccarty/cloudcam/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.mccarty.cloudcam.utils;
+
+/**
+ * Created by Larry McCarty on 10/28/2018.
+ */
+
+public class Constants {
+    public static final String AWS_IMAGE_PATH = "device-inventory-media/media/images/";
+}
