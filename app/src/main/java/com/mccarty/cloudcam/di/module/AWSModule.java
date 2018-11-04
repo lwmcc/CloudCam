f0296791a1617a07fb6dc5323988ff4a2d18ff44
Larry <lwmccarty@gmail.com>
2018-11-04 02:24:12 -0700

    Provide user pool and transfer utility for injection

---
+++ app/src/main/java/com/mccarty/cloudcam/di/module/AWSModule.java
@@ -0,0 +1,133 @@
+package com.mccarty.cloudcam.di.module;
+
+import android.app.Application;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.AmazonWebServiceRequest;
+import com.amazonaws.ResponseMetadata;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.AttributeValueUpdate;
+import com.amazonaws.services.dynamodbv2.model.BatchGetItemRequest;
+import com.amazonaws.services.dynamodbv2.model.BatchGetItemResult;
+import com.amazonaws.services.dynamodbv2.model.BatchWriteItemRequest;
+import com.amazonaws.services.dynamodbv2.model.BatchWriteItemResult;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+import com.amazonaws.services.dynamodbv2.model.CreateBackupRequest;
+import com.amazonaws.services.dynamodbv2.model.CreateBackupResult;
+import com.amazonaws.services.dynamodbv2.model.CreateGlobalTableRequest;
+import com.amazonaws.services.dynamodbv2.model.CreateGlobalTableResult;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
+import com.amazonaws.services.dynamodbv2.model.DeleteBackupRequest;
+import com.amazonaws.services.dynamodbv2.model.DeleteBackupResult;
+import com.amazonaws.services.dynamodbv2.model.DeleteItemRequest;
+import com.amazonaws.services.dynamodbv2.model.DeleteItemResult;
+import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
+import com.amazonaws.services.dynamodbv2.model.DeleteTableResult;
+import com.amazonaws.services.dynamodbv2.model.DescribeBackupRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeBackupResult;
+import com.amazonaws.services.dynamodbv2.model.DescribeContinuousBackupsRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeContinuousBackupsResult;
+import com.amazonaws.services.dynamodbv2.model.DescribeGlobalTableRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeGlobalTableResult;
+import com.amazonaws.services.dynamodbv2.model.DescribeGlobalTableSettingsRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeGlobalTableSettingsResult;
+import com.amazonaws.services.dynamodbv2.model.DescribeLimitsRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeLimitsResult;
+import com.amazonaws.services.dynamodbv2.model.DescribeTableRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeTableResult;
+import com.amazonaws.services.dynamodbv2.model.DescribeTimeToLiveRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeTimeToLiveResult;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
+import com.amazonaws.services.dynamodbv2.model.GetItemResult;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeysAndAttributes;
+import com.amazonaws.services.dynamodbv2.model.ListBackupsRequest;
+import com.amazonaws.services.dynamodbv2.model.ListBackupsResult;
+import com.amazonaws.services.dynamodbv2.model.ListGlobalTablesRequest;
+import com.amazonaws.services.dynamodbv2.model.ListGlobalTablesResult;
+import com.amazonaws.services.dynamodbv2.model.ListTablesRequest;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
+import com.amazonaws.services.dynamodbv2.model.ListTagsOfResourceRequest;
+import com.amazonaws.services.dynamodbv2.model.ListTagsOfResourceResult;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.dynamodbv2.model.QueryRequest;
+import com.amazonaws.services.dynamodbv2.model.QueryResult;
+import com.amazonaws.services.dynamodbv2.model.RestoreTableFromBackupRequest;
+import com.amazonaws.services.dynamodbv2.model.RestoreTableFromBackupResult;
+import com.amazonaws.services.dynamodbv2.model.RestoreTableToPointInTimeRequest;
+import com.amazonaws.services.dynamodbv2.model.RestoreTableToPointInTimeResult;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.dynamodbv2.model.TagResourceRequest;
+import com.amazonaws.services.dynamodbv2.model.UntagResourceRequest;
+import com.amazonaws.services.dynamodbv2.model.UpdateContinuousBackupsRequest;
+import com.amazonaws.services.dynamodbv2.model.UpdateContinuousBackupsResult;
+import com.amazonaws.services.dynamodbv2.model.UpdateGlobalTableRequest;
+import com.amazonaws.services.dynamodbv2.model.UpdateGlobalTableResult;
+import com.amazonaws.services.dynamodbv2.model.UpdateGlobalTableSettingsRequest;
+import com.amazonaws.services.dynamodbv2.model.UpdateGlobalTableSettingsResult;
+import com.amazonaws.services.dynamodbv2.model.UpdateItemRequest;
+import com.amazonaws.services.dynamodbv2.model.UpdateItemResult;
+import com.amazonaws.services.dynamodbv2.model.UpdateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.UpdateTableResult;
+import com.amazonaws.services.dynamodbv2.model.UpdateTimeToLiveRequest;
+import com.amazonaws.services.dynamodbv2.model.UpdateTimeToLiveResult;
+import com.amazonaws.services.dynamodbv2.model.WriteRequest;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.mccarty.cloudcam.R;
+import com.mccarty.cloudcam.apis.AWSDb;
+
+import java.util.List;
+import java.util.Map;
+
+import dagger.Module;
+import dagger.Provides;
+
+import static com.mccarty.cloudcam.utils.Constants.AWS_IMAGE_PATH;
+
+/**
+ * Created by Larry McCarty on 10/28/2018.
+ */
+
+@Module
+public class AWSModule {
+
+    @Provides
+    static BasicAWSCredentials provideBasicAWSCredentials(Application application) {
+        return new BasicAWSCredentials(application.getString(R.string.AWS_ACCESS_KEY),
+                application.getString(R.string.AWS_SECRET_KEY));
+    }
+
+    @Provides
+    static AmazonS3Client provideAmazonS3Client(BasicAWSCredentials basicAWSCredentials) {
+        return new AmazonS3Client(basicAWSCredentials);
+    }
+
+    @Provides
+    static TransferUtility provideTransferUtility(Application application, AmazonS3Client s3) {
+        return TransferUtility.builder().context(application).awsConfiguration(AWSMobileClient.getInstance().
+                getConfiguration()).s3Client(s3).build();
+    }
+
+    @Provides
+    static CognitoUserPool providedCognitoUserPool(Application application) {
+        return new CognitoUserPool(application,  AWSMobileClient.getInstance().getConfiguration())  ;
+    }
+
+}
