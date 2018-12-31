e51570679d3dc07b645521e0d34047d11599e8c8
Larry <lwmccarty@gmail.com>
2018-12-30 21:02:29 -0700

    Add DAO, get remote images if no local images, save to db, then get them

--- app/src/main/java/com/mccarty/cloudcam/model/MainModel.java
+++ app/src/main/java/com/mccarty/cloudcam/model/MainModel.java
@@ -1,51 +1,43 @@
 package com.mccarty.cloudcam.model;
 
-
-import android.app.Application;
-import android.content.Context;
-
-import com.amazonaws.mobile.auth.core.IdentityManager;
-import com.mccarty.cloudcam.di.module.AWSModule;
-import com.mccarty.cloudcam.di.module.NetworkModule;
 import com.mccarty.cloudcam.persistence.local.CloudCamDatabase;
+import com.mccarty.cloudcam.persistence.local.Image.ImageDao;
 import com.mccarty.cloudcam.persistence.local.Image.ImageEntity;
 import com.mccarty.cloudcam.persistence.remote.image.RemoteImageDao;
 import com.mccarty.cloudcam.utils.NetworkUtils;
 
 import java.util.List;
 
-import io.reactivex.Single;
-
 public class MainModel {
 
     private final CloudCamDatabase database;
     private final NetworkUtils networkUtils;
     private final RemoteImageDao remoteImageDao;
+    private final ImageDao imageDao;
 
-    public MainModel(CloudCamDatabase cloudDatabase, NetworkUtils networkUtils, RemoteImageDao remoteImageDao) {
+    public MainModel(CloudCamDatabase cloudDatabase, NetworkUtils networkUtils, RemoteImageDao remoteImageDao,
+                     ImageDao imageDao) {
         this.database = cloudDatabase;
         this.networkUtils = networkUtils;
         this.remoteImageDao = remoteImageDao;
+        this.imageDao = imageDao;
     }
 
     /**
-     * Get all local images
+     * Get all local or remote images
+     *
      * @return List
      */
     public List<ImageEntity> getAllImages() {
+        if (!database.imageDao().getAll().isEmpty()) {
+            return database.imageDao().getAll();
+        } else {
+            remoteImageDao.getImages().forEach(imageDao::insertImage);
+        }
         return database.imageDao().getAll();
-        // TODO:
-        // if empty query remote db for iamges
-        // download remote images
-        // insert them into local db
-        // show them on UI
     }
 
-    public void scanForRemoteImages() {
-       remoteImageDao.getImages();
-    }
-
-    public boolean canAccessAWS() {
-        return  networkUtils.hasNetworkAccess() && IdentityManager.getDefaultIdentityManager().isUserSignedIn();
+    public boolean hasInternetAccess() {
+        return networkUtils.hasNetworkAccess();
     }
 }
