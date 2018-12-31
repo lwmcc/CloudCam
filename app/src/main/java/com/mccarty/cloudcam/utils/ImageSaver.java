f862a4fa14c5bb955db78e0cd6b62e7438eab591
Larry <lwmccarty@gmail.com>
2018-12-30 20:47:07 -0700

    Clean up lint warning

--- app/src/main/java/com/mccarty/cloudcam/utils/ImageSaver.java
+++ app/src/main/java/com/mccarty/cloudcam/utils/ImageSaver.java
@@ -17,7 +17,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.time.Instant;
 import java.util.Date;
 import java.util.UUID;
 
