0a625dedbe1ccfa28524afed579c044e8cacfaed
Larry <lwmccarty@gmail.com>
2018-12-30 21:03:25 -0700

    Some refactoring

--- app/src/main/java/com/mccarty/cloudcam/persistence/local/CloudCamDatabase.java
+++ app/src/main/java/com/mccarty/cloudcam/persistence/local/CloudCamDatabase.java
@@ -4,11 +4,9 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
 
+import com.mccarty.cloudcam.persistence.Converters;
 import com.mccarty.cloudcam.persistence.local.Image.ImageDao;
 import com.mccarty.cloudcam.persistence.local.Image.ImageEntity;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
 
 @Database(entities = {ImageEntity.class}, version = 1)
 @TypeConverters({Converters.class})
