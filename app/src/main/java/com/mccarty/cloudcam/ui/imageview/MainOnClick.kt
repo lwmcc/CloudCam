cb46dfd87c94613ffba69ff2f790ad12a844225b
Larry <lwmccarty@gmail.com>
2019-01-10 00:44:35 -0700

    Add Click listener interface for ImageAdapter

---
+++ app/src/main/java/com/mccarty/cloudcam/ui/imageview/MainOnClick.kt
@@ -0,0 +1,10 @@
+package com.mccarty.cloudcam.ui.imageview
+
+import android.view.View
+
+/**
+ * Created by Larry McCarty on 1/9/2019.
+ */
+interface MainOnClick {
+    fun onClick(view: View, position: Int)
+}
