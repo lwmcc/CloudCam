c452f422bfa5451008fb2ebf965080f420cd5a0e
Larry <lwmccarty@gmail.com>
2019-01-13 02:25:10 -0700

    Add annotation

--- app/src/main/java/com/mccarty/cloudcam/ui/components/ViewClickListener.kt
+++ app/src/main/java/com/mccarty/cloudcam/ui/components/ViewClickListener.kt
@@ -5,6 +5,7 @@ import android.view.View
 /**
  * Created by Larry McCarty on 1/8/2019.
  */
+@FunctionalInterface
 public interface ViewClickListener {
     fun onClick(view: View, position: Int)
 }
